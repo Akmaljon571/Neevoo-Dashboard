9a1b066eb3a7d6047c377f5d0c7665a0599bd120
Sherzod <sh.azamatugli2110@gamil.com>
2023-05-18 09:22:28 +0500

    course

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Xisobot } from "../page";
+import Course from "../page/Course/course";
 
 function Routers() {
-    return (  
-        <Routes>
-            <Route path="/" element={<Xisobot />} />
-            {/* <Route path="/" element={} /> */}
-        </Routes>
-
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Xisobot />} />
+      <Route path="/course" element={<Course />} />
+    </Routes>
+  );
 }
 
 export default Routers;
