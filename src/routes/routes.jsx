9cf8c9aee046d5c87948a0af70f98af8f510010f
simlenodir <nodirsmailov0@gmail.com>
2023-05-18 09:40:52 +0300

    categories started but not finnished

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Xisobot } from "../page";
+import Category from "../page/category";
 
 function Routers() {
-    return (  
-        <Routes>
-            <Route path="/" element={<Xisobot />} />
-            {/* <Route path="/" element={} /> */}
-        </Routes>
-
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Xisobot />} />
+      <Route path="/category" element={<Category />} />
+    </Routes>
+  );
 }
 
 export default Routers;
